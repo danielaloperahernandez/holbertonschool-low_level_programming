511b50779e7124b884fefaa24aa513aa168cf733
danielaloperahernandez <1469@holbertonschool.com>
2020-02-06 00:03:45 +0000

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * printf - Print text on the screen
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-
-  printf("with proper grammar, but the outcome is a piece of art\n");
-  return(0);
-
+printf("with proper grammar, but the outcome is a piece of art\n");
+return (0);
 }
