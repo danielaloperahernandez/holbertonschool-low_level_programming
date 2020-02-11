9e7b69c8a4a80b45c395694a684ebe64c4eaed23
danielaloperahernandez <1469@holbertonschool.com>
2020-02-11 18:33:58 +0000

    0 - main.c

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* HOLBERTON_H */#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* HOLBERTON_H */
