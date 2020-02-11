f2641c0a21ac7451b9c93e02ecf9a8bb7c793a96
danielaloperahernandez <1469@holbertonschool.com>
2020-02-11 02:57:01 +0000

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * main - main block
+ * printf - print greater, zero, less
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
