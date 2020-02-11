1158eda50e69ce62441bc5429065c8d8ef84a640
danielaloperahernandez <1469@holbertonschool.com>
2020-02-11 21:56:42 +0000

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,43 +8,41 @@ void print_times_table(int n)
 {
 	int row, column;
 
-	if (n < 15 && n => 0)
+	if (n < 15 && n >= 0)
 	{
-	for (row = 0; row <= n; row++)
-	{
-		for (column = 0; column <= n; column++)
+		for (row = 0; row <= n; row++)
 		{
-		if (column != 0)
-		{
-			if ((row * column) <= 9)
+			for (column = 0; column <= n; column++)
 			{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar((row * column) + '0');
+				if (column != 0)
+				{
+					_putchar(',');
+					if ((row * column) <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar((row * column) + '0')
+					}
+					else if ((row * column) <= 99)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(((row * column) / 10) + '0');
+						_putchar(((row * column) % 10) + '0');
+					}
+					else if ((row * column) <= 999)
+					{
+						_putchar(' ');
+						_putchar(((row * column) / 100) + '0');
+						_putchar((((row * column) / 10) % 10) + '0');
+						_putchar(((row * column) % 10) + '0');
+					}
+				}
+				else
+					_putchar('0');
 			}
-			else if ((row * column) <= 99)
-			{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(((row * column) / 10) + '0');
-			_putchar(((row * column) % 10) + '0');
-			}
-			else if ((row * column) <= 999)
-			{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(((row * column) / 100) + '0');
-			_putchar((((row * column) / 10) % 10) + '0');
-			_putchar(((row * column) % 10) + '0');
-			}
+			_putchar('\n');
 		}
-		else
-			_putchar((row * column) + '0');
-		}
-		_putchar('\n');
-	}
 	}
 }
