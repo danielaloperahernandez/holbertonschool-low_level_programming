38bca5381e369ae05b0fda1efdbd90e33031719e
danielaloperahernandez <1469@holbertonschool.com>
2020-02-12 00:00:03 +0000

    3-print_remaining_days.c

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -5,7 +5,7 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-int covert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
+int covert_day(int month, int day);
 
 #endif /* HOLBERTON_H */
