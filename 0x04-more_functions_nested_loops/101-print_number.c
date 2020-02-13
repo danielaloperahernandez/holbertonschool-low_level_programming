87db8ac7b42854a855f21c413a6de81ae00e716e
danielaloperahernandez <1469@holbertonschool.com>
2020-02-13 20:03:31 +0000

    101-print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,13 +8,17 @@ void print_number(int n)
 {
 	int len, powten, i, digit, num;
 
-	num = n;
-
 	if (n == 0)
 		_putchar('0');
 	else
 	{
+		if (n < 0)
+		{
+			n *= -1;
+			_putchar('-');
+		}
 		len = 0;
+		num = n;
 		while (num != 0)
 		{
 			num /= 10;
@@ -24,11 +28,6 @@ void print_number(int n)
 		for (i = 1; i <= len - 1; i++)
 		{
 			powten *= 10;
-		}
-		if (n < 0)
-		{
-			_putchar('-');
-			n *= -1;
 		}
 		for (i = 1; i <= len; i++)
 		{
