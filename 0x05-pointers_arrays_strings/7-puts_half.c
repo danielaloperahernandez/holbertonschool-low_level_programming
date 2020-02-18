e7de14901844925de0041f47a41f7b7c893aef36
danielaloperahernandez <1469@holbertonschool.com>
2020-02-18 15:45:00 +0000

    7 exercise

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -27,9 +27,8 @@ void puts_half(char *str)
 
 	i = _strlen(str);
 	if (i % 2 != 0)
-		i = (i - 1) / 2;
-	else
-		i = i / 2;
+		i++;
+	i = i / 2;
 	while (str[i] != '\0')
 		_putchar(str[i++]);
 	_putchar('\n');
