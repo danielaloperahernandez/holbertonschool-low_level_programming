58e7d654998ca5b25de106cf814f7f46ba9280e4
danielaloperahernandez <1469@holbertonschool.com>
2020-02-20 23:05:31 +0000

    Exercise 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,12 +10,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; (i < n) && (*(dest + i) != '\0'); i++)
-	{
-		if ((*src + i) != '\0')
-			*(dest + i) = *(src + i);
-		else
-			return (dest);
-	}
+	for (i = 0; (i < n) && (*(src + i) != '\0'); i++)
+		*(dest + i) = *(src + i);
+	for (; i < n; i++)
+		*(dest + i) = '\0';
 	return (dest);
 }
