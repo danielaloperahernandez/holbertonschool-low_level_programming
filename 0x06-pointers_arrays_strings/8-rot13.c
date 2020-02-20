ce9560c22f461f8a0c2958aba9df9d483d692f7a
danielaloperahernandez <1469@holbertonschool.com>
2020-02-20 23:29:34 +0000

    rot 13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,15 +1,26 @@
 #include "holberton.h"
 /**
-*
-*
+* rot13 - function that encodes a string
+* @s: string to encode
+* Return: s
 */
 char *rot13(char *s)
 {
-	int i;
+	int i, j;
+
+	char c[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; *(s + i) != '\0'; i++)
-	{ 
-		if (i >
+	{
+		for (j = 0; *(s + j) != '\0'; j++)
+		{
+			if (*(s + i) == c[j])
+			{
+				*(s + i) = rot[j];
+				break;
+			}
+		}
 	}
-return (s);
+	return (s);
 }
