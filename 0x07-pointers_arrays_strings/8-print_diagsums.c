28e9bb3fe34943a94b726cab0ed7706b745a63ad
danielaloperahernandez <1469@holbertonschool.com>
2020-02-24 20:48:01 +0000

    8-print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
-*print_diagsums -  sets the value of a pointer to a char.
+*print_diagsums - sets the value of a pointer to a char.
 *@a: string
 *@size: size
 *Description: print diagonal sum
@@ -9,15 +9,11 @@
 
 void print_diagsums(int *a, int size)
 {
-	int sum1 = 0, sum2 = 0, i, j, p;
+	int sum1 = 0, sum2 = 0, i, j;
 
-	for (i = 0; i < size; i++)
-		sum1 += (a + i)[j];
-	for (j = 0; j < size; j++)
-	{
-		p = j + size - 1;
-		sum2 += (a + p)[j];
-		size--;
-	}
+	for (i = 0; i < size; ++i)
+		sum1 += (a + i * size)[i];
+	for (j = 0; j < size; ++j)
+		sum2 += (a + j * size)[--i];
 	printf("%d, %d\n", sum1, sum2);
 }
