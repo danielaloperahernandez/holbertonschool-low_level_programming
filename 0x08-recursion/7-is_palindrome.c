de6903a003fc2480310b2fec19af78a3f8627b5a
danielaloperahernandez <1469@holbertonschool.com>
2020-02-26 21:38:00 +0000

    7-is_palindrome.c

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -6,30 +6,30 @@
 */
 int _strlen_recursion(char *s)
 {
-        if (*s == '\0')
-                return (0);
-        return (1 + _strlen_recursion(s + 1));
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
-
 #include "holberton.h"
 /**
 *is_palindrome2 - returns 1 if a string is a palindrome and 0 if not
 *@s: string
+*@rev: string reverse
 *Return: 1 or 0
 */
 int is_palindrome2(char *s, char *rev)
 {
 	if (*s != *rev)
 		return (0);
-	if (*s >= *rev)
+	if (*s == *rev)
 		return (1);
-	return (is_palindrome2(++s, --rev);
+	return (is_palindrome2(++s, --rev));
 }
 #include "holberton.h"
 /**
 *is_palindrome - returns 1 if a string is a palindrome and 0 if not
 *@s: string
-*Return: 1 or 0 
+*Return: 1 or 0
 */
 int is_palindrome(char *s)
 {
