42169d6339e5b23efb14ab02bee8686756606d5d
danielaloperahernandez <1469@holbertonschool.com>
2020-03-03 19:38:47 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,20 +11,20 @@ char *str_concat(char *s1, char *s2)
 	char *cat;
 	int i, j, k;
 
-	for (i = 0; s1[i]; ++i)
+	for (i = 0; s1 && s1[i]; ++i)
 		;
-	for (j = 0; s2[j]; ++j)
+	for (j = 0; s2 && s2[j]; ++j)
 		;
 	cat = (char *)malloc((i + j + 1) * sizeof(char));
 	if (cat == NULL)
 		return (NULL);
 	k = 0;
-	for (i = 0; s1[i]; ++i)
+	for (i = 0; s1 && s1[i]; ++i)
 	{
 		cat[k] = s1[i];
 		++k;
 	}
-	for (j = 0; s2[j]; ++j)
+	for (j = 0; s2 && s2[j]; ++j)
 	{
 		cat[k] = s2[j];
 		++k;
