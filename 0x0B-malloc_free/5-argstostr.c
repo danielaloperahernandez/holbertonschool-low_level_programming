40916ddd0abcc393faafa4ba87b22564b1103cf9
danielaloperahernandez <1469@holbertonschool.com>
2020-03-04 00:50:18 +0000

    5-argstostr.c

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*size - obtain the size of an array of 2 dimensions
+*@c: number of arguments
+*@v: array of pointers to strings
+*Return: size of an array
+*/
+int size(int c, char **v)
+{
+	int rec1, rec2, sz = 0;
+
+	for (rec1 = 0; rec1 < c; rec1++)
+	{
+		for (rec2 = 0; v[rec1][rec2]; rec2++)
+		{
+			sz++;
+		}
+		sz = sz + 2;
+	}
+	sz++;
+	return (sz);
+}
+/**
+*argstostr - concatenates all the arguments of your program
+*@ac: number of arguments
+*@av: array of pointers to strings
+*Return: pointer to a new string or NULL
+*/
+char *argstostr(int ac, char **av)
+{
+	char *a;
+	int i, j, k, len;
+
+	if (ac == 0 || av == 0)
+		return (NULL);
+	len = size(ac, av);
+	a = malloc(len * sizeof(char));
+	if (a == NULL)
+	{
+		free(a);
+		return (NULL);
+	}
+	k = 0;
+	for (i = 1; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			a[k] = av[i][j];
+			k++;
+		}
+		a[k] = '\n';
+		k++;
+		a[k] = '\0';
+	}
+	return (a);
+}
