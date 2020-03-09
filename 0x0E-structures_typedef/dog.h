70346cbba570167ee978de5c4985bb5bc7bf8ffb
danielaloperahernandez <1469@holbertonschool.com>
2020-03-09 22:47:00 +0000

    update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,8 +5,6 @@
  * @name: First member
  * @age: Second member
  * @owner: Third member
- *
- * Description: Longer description
  */
 typedef struct dog
 {
@@ -18,4 +16,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif /*DOG_H*/
+#endif /*_DOG_H_*/
