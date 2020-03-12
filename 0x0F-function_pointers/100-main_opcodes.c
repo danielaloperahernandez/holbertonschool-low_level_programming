3014963712a93a6560598f44192bce362218d308
danielaloperahernandez <1469@holbertonschool.com>
2020-03-12 23:12:26 +0000

    first commit of 100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - print opcodes of main fuction
+*@argc: number of command lines arguments
+*@argv: array of command line arguments
+*Return: 1, 2 or EXIT_SUCCESS
+*/
+int main(int argc, char *argv[])
+{
+	short nbytes, i;
+
+	if (argc != 2)
+	{
+		puts("Error");
+		exit(1);
+	}
+	nbytes = atoi(argv[1]);
+	if (nbytes < 0)
+	{
+		puts("Error");
+		exit(2);
+	}
+	if (nbytes == 0)
+	{
+		putchar('\n');
+		exit(EXIT_SUCCESS);
+	}
+	printf("%2x", *((unsigned char *)(main)));
+	for (i = 1; i < nbytes; ++i)
+		printf("%02x", *((unsigned char *)(main + i)));
+	putchar('\n');
+	exit(EXIT_SUCCESS);
+}
