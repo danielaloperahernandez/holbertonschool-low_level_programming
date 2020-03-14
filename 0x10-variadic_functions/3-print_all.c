a775686ee4e4a9ff871dc5e0931d6c6f318bc077
danielaloperahernandez <1469@holbertonschool.com>
2020-03-14 18:27:27 +0000

    delete else 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -31,9 +31,8 @@ void print_all(const char * const format, ...)
 				case 's':
 					s = va_arg(ap, char *);
 					if (s == NULL)
-						printf("(nil)");
-					else
-						printf("%s", s);
+						s = "(nil)";
+					printf("%s", s);
 					break;
 				default:
 					continue;
