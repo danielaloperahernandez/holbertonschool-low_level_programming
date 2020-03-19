2c3a2e5fd4979aa9ea9869d0803c541219716400
danielaloperahernandez <1469@holbertonschool.com>
2020-03-19 17:37:13 +0000

    Add directory 0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "list.h"
+#include <stdio.h>
+/**
+*print_list - print string and length of string for each element of 'h'
+*@h: pointer to first element of list
+*Return: length of list
+*/
+size_t print_list(const list_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		++size;
+	}
+	return (size);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,4 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+size_t print_list(const list_t *h);
+#endif/*LIST_H_*/
