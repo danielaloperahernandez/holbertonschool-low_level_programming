97e3a17992578474f79118172225843eb5594a6a
danielaloperahernandez <1469@holbertonschool.com>
2020-03-20 00:33:59 +0000

    Update lists.h 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+*free_list - free memory of all nodes of list pointed
+*@head: pointer to beggining of list
+*/
+void free_list(list_t *head)
+{
+	list_t *next;
+
+	while (head)
+	{
+		next = head->next;
+		free(head->str);
+		free(head);
+		head = next;
+	}
+}
