b1556dd46f19bfda1e34515e567dcab0a6a31e96
danielaloperahernandez <1469@holbertonschool.com>
2020-03-26 20:35:21 +0000

    Add 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,13 @@
+/**
+*clear_bit - sets the value of a bit to 0 at a given index
+*@n: number to change
+*@index: index to search
+*Return: 1 if it worked, or -1 if an error occurred
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(*n) * 8)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
