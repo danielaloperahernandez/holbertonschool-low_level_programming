5ce30140dd3d8c4aeb2a9be54da5a718ea287098
danielaloperahernandez <1469@holbertonschool.com>
2020-03-27 18:06:32 +0000

    Update sizeof 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,7 +6,7 @@
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(*n) * 8)
+	if (index > ((sizeof(*n) * 8) - 1))
 		return (-1);
 	*n &= ~(1 << index);
 	return (1);
