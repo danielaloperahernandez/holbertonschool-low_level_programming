7ea2ce223832d74435827a39d3cfdcc6c02815d0
danielaloperahernandez <1469@holbertonschool.com>
2020-03-31 17:45:00 +0000

    Correction of O_WRONLY

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	for (i = 0; text_content && text_content[i]; i++)
 		;
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY | S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (i)
