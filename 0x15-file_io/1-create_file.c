b3bc4517b06b2741ec75f7c7da9067409b261826
danielaloperahernandez <1469@holbertonschool.com>
2020-03-31 17:42:13 +0000

    Add 1-create_file.c and respective prototype

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+/**
+*create_file - function that creates a file
+*@filename: path of a file to create
+*@text_content: content to insert into file
+*Return: 1 on success, -1 if error occurs
+*/
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t i = 0;
+	ssize_t w = 0;
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	for (i = 0; text_content && text_content[i]; i++)
+		;
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (i)
+		w = write(fd, text_content, i);
+	if (w == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -2,4 +2,5 @@
 #define _HOLBERTON_H_
 #include <unistd.h>
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif /*_HOLBERTON_H_*/
