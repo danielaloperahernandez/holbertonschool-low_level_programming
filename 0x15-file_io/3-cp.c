690daba334ce3856afe9424f61e33c7efc97f064
danielaloperahernandez <1469@holbertonschool.com>
2020-03-31 22:50:52 +0000

    3-cp.c logical fails

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,38 +1,16 @@
 #include "holberton.h"
 /**
- * main - copy content of one file into another
- * @argc: count of arguments
- * @argv: array of arguments
- *
- * Return: EXIT_SUCCESS on success or exit with error number.
- */
-int main(int argc, char *argv[])
+*error_wr - detect error in write or read
+*@fdr: result of open file to read
+*@fdw: result of open file to write
+*@file_from: file from copy
+*@file_to: file to copy
+*/
+void error_wr(int fdr, int fdw, char *file_from, char *file_to)
 {
-	char *file_from, *file_to;
+	ssize_t r, w;
 	char buf[NBYTES];
-	ssize_t r = 0, w = 0;
-	int fdr, fdw;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	file_from = argv[1];
-	file_to = argv[2];
-	fdr = open(file_from, O_RDONLY);
-	if (fdr == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-			file_from);
-		exit(98);
-	}
-	fdw = open(file_to, O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (fdw == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
 	do {
 		r = read(fdr, buf, NBYTES);
 		if (r == -1)
@@ -50,6 +28,39 @@ int main(int argc, char *argv[])
 			}
 		}
 	} while (r);
+}
+/**
+ * main - copy content of one file into another
+ * @argc: count of arguments
+ * @argv: array of arguments
+ *
+ * Return: EXIT_SUCCESS on success or exit with error number.
+ */
+int main(int argc, char *argv[])
+{
+	char *file_from, *file_to;
+	int fdr, fdw;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_from = argv[1];
+	file_to = argv[2];
+	fdr = open(file_from, O_RDONLY);
+	if (fdr == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	fdw = open(file_to, O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	error_wr(fdr, fdw, file_from, file_to);
+	if (fdw == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
 	if (close(fdw))
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdw);
