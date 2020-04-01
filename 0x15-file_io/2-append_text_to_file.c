92c0da9ba120b71dac2d0ba1b7b4a7a63a7c6f24
danielaloperahernandez <1469@holbertonschool.com>
2020-04-01 00:56:36 +0000

    Changes to return 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (write(fd, text_content, size) != size)
 	{
 		close(fd);
-		return (0);
+		return (-1);
 	}
 	close(fd);
 	return (1);
