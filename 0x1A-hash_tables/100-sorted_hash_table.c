2ffe4440ebfdefa31642e811ade42f7cce053a77
daniela <1469@holbertonschool.com>
2020-05-29 18:09:51 +0000

    Replacing malloc with calloc

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -98,7 +98,7 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 		}
 		bucket = bucket->next;
 	}
-	new_node = malloc(sizeof(shash_node_t));
+	new_node = calloc(1, sizeof(shash_node_t));
 	if (new_node == NULL)
 	{
 		free(valuecopy);
