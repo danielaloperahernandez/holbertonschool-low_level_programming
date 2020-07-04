f9eee14d5f514cd6a83046ca4adef2b5ccabfeb4
daniela <1469@holbertonschool.com>
2020-07-04 02:19:17 +0000

    Fixing range for loops

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,8 +8,8 @@ def island_perimeter(grid):
     max_row = len(grid)
     perimeter = 0
 
-    for i in range(0, max_row - 1):
-        for j in range(0, max_col - 1):
+    for i in range(0, max_row):
+        for j in range(0, max_col):
             if grid[i][j] == 1:
                 if i-1 < 0 or grid[i-1][j] == 0:
                     perimeter += 1
